769fcbed54b4bd1a241b105ad05265a0cee45dc5
Rodrigo <rodriqet17@gmail.com>
2026-03-04 13:23:00 +0100

    Implementacion del codigo de las clases creadas

--- src/main/java/com/amongus/DAOImpl/SalaDAOImpl.java
+++ src/main/java/com/amongus/DAOImpl/SalaDAOImpl.java
@@ -1,8 +1,31 @@
 package com.amongus.DAOImpl;
 
-public class SalaDAOImpl {
+import com.amongus.DAO.SalaDAO;
+import com.amongus.modelo.Sala;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class SalaDAOImpl implements SalaDAO {
+
+    private Connection conexion;
+
+    public SalaDAOImpl(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    @Override
+    public void insertar(Sala sala){
+        String sql = "INSERT INTO tarea (nombre, peso) VALUES (?, ?)";
+        try (PreparedStatement ps = conexion.prepareStatement(sql)) {
+            ps.setString(1, sala.getNombre());
+            ps.setInt(2, sala.getPeso());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 
--- src/main/java/com/amongus/modelo/Tarea.java
+++ src/main/java/com/amongus/modelo/Tarea.java
@@ -2,10 +2,72 @@ package com.amongus.modelo;
 
 public class Tarea {
 
+    private int id;
 
+    private String descripcion;
 
+    private boolean completada;
 
+    private Tripulante tripulanteAsignado;
 
+    private Sala sala;
 
+    public Tarea(String descripcion, Tripulante tripulante, Sala sala){
+        this.descripcion = descripcion;
+        completada = false;
+        this.tripulanteAsignado = tripulante;
+        this.sala = sala;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    public Tripulante getTripulanteAsignado() {
+        return tripulanteAsignado;
+    }
+
+    public void setTripulanteAsignado(Tripulante tripulanteAsignado) {
+        this.tripulanteAsignado = tripulanteAsignado;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "id=" + id +
+                ", descripcion='" + descripcion + '\'' +
+                ", completada=" + completada +
+                ", tripulanteAsignado=" + tripulanteAsignado +
+                ", sala=" + sala +
+                '}';
+    }
 
 }
