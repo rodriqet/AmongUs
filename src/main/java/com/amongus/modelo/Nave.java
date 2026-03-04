cba3700da155b7477dd8d90c79d0596cf8f1871b
Rodrigo <rodriqet17@gmail.com>
2026-03-04 14:28:47 +0100

    Nave y tarea cambios

--- src/main/java/com/amongus/modelo/Nave.java
+++ src/main/java/com/amongus/modelo/Nave.java
@@ -1,8 +1,12 @@
 package com.amongus.modelo;
 
+import com.amongus.modelo.tripulante.Impostor;
+import com.amongus.modelo.tripulante.Tripulante;
+
 import java.util.ArrayList;
 
 public class Nave {
+
     private ArrayList<Tripulante> tripulantes;
 
     private ArrayList<Sala> salas;
@@ -26,10 +30,51 @@ public class Nave {
         return tareas;
     }
 
-//• agregarTarea(Tarea): Añade una tarea.
-//• limpiarPantalla(): Ejecuta System.out.print("\033[H\033[2J") + flush().
-//• mostrarEstadoNave(): Muestra tripulantes, salas y progreso de tareas.
-//• iniciarVotacion(): Gestiona la votación con limpieza de pantalla entre votos.
+    public void agregarTarea(Tarea tarea){
+        tareas.add(tarea);
+    }
+
+    public void limpiarPantalla(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public void mostrarEstadoNave(){
+        for (Tripulante tripulante : tripulantes) {
+            System.out.println(tripulante.toString());
+        }
+        for (Sala sala : salas) {
+            System.out.println(sala.toString());
+        }
+        for (Tarea tarea : tareas) {
+            System.out.println(tarea.toString());
+        }
+    }
+
+    //• iniciarVotacion(): Gestiona la votación con limpieza
+    // de pantalla entre votos.
+    public void iniciarVotacion(){
+
+    }
+
+    public boolean verificarVictoriaTripulantes(){
+        boolean victoriaPorExpulsion = false;
+
+        for (Tripulante impostor : tripulantes) {
+            if (impostor instanceof Impostor){
+                victoria = false;
+                break;
+            }
+        }
+
+
+        if (tareas.isEmpty()){
+            victoria = true;
+        }
+    }
+
+
+
 //• verificarVictoriaTripulantes(): boolean — true si tareas completas o impostor
 //expulsado.
 //• verificarVictoriaImpostor(): boolean — true si impostores vivos ≥ normales vivos.
