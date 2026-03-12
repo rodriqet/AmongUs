c13b670222474e83a58ca6123dabb02c71d869f3
David <davidlopezruiz222@gmail.com>
2026-03-12 10:46:06 +0100

    El tripulanteDAO terminado y main completado

--- src/main/java/com/amongus/Main.java
+++ src/main/java/com/amongus/Main.java
@@ -27,179 +27,189 @@ public class Main {
             TripulanteDAO tripulanteDAO = new TripulanteDAOImpl(conexion);
             TareaDAO tareaDAO = new TareaDAOImpl(conexion);
             SalaDAO salaDAO = new SalaDAOImpl(conexion);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
 
-        int numTripulantes = 0;
-        while (numTripulantes >= 4 && numTripulantes <= 10) {
-            System.out.print("Cuantos jugadores van a jugar? (mínimo 4, máximo 10): ");
-            numTripulantes = sc.nextInt();
-            sc.nextLine();
-        }
-
-        ArrayList<String> jugadores = new ArrayList<>();
-        for (int i = 0; i < numTripulantes; i++) {
-            System.out.print("El " + i + " jugador como se llama: ");
-            jugadores.add(sc.nextLine());
-        }
-
-        ArrayList<Tripulante> tripulantes = new ArrayList<>();
-
-        Collections.shuffle(jugadores);
-        Capitan capitan = new Capitan(jugadores.removeFirst());
-        tripulantes.add(capitan);
-        Collections.shuffle(jugadores);
-        Impostor impostor = new Impostor(jugadores.removeFirst());
-        tripulantes.add(impostor);
-
-        for (int i = 0; i < tripulantes.size(); i++) {
-            if (i%2 == 0) {
-                Medico medico = new Medico(jugadores.removeFirst());
-                tripulantes.add(medico);
-            } else {
-                Ingeniero ingeniero = new Ingeniero(jugadores.removeFirst());
-                tripulantes.add(ingeniero);
+            int numTripulantes = 0;
+            while (numTripulantes >= 4 && numTripulantes <= 10) {
+                System.out.print("Cuantos jugadores van a jugar? (mínimo 4, máximo 10): ");
+                numTripulantes = sc.nextInt();
+                sc.nextLine();
             }
-        }
 
-        ArrayList<Sala> salas = new ArrayList<>();
-        ArrayList<String> NomSalas = new ArrayList<>();
-        Collections.addAll(NomSalas, "Reactor", "Cafeteria", "Navegación", "Electricidad", "Armamento", "Comunicaciones");
+            ArrayList<String> jugadores = new ArrayList<>();
+            for (int i = 0; i < numTripulantes; i++) {
+                System.out.print("El " + i + " jugador como se llama: ");
+                jugadores.add(sc.nextLine());
+            }
 
-        for (int i = 0; i < NomSalas.size(); i++) {
-            Sala sala = new Sala(NomSalas.get(i));
-            salas.add(sala);
-        }
+            ArrayList<Tripulante> tripulantes = new ArrayList<>();
 
-        //TODO Persistencia inicial: Insertar tripulantes y salas en BBDD
+            Collections.shuffle(jugadores);
+            Capitan capitan = new Capitan(jugadores.removeFirst());
+            tripulantes.add(capitan);
+            Collections.shuffle(jugadores);
+            Impostor impostor = new Impostor(jugadores.removeFirst());
+            tripulantes.add(impostor);
 
-        Nave nave = new Nave(tripulantes, salas);
-
-        HashMap<String, Sala> tareaDesc = new HashMap<>();
-        // 0: Reactor, 1: Cafeteria, 2: Navegación, 3: Electricidad, 4: Armamento, 5: Comunicaciones
-
-        // --- REACTOR (Índice 0) ---
-        tareaDesc.put("Arrancar reactor", salas.get(0));
-        tareaDesc.put("Desbloquear colectores", salas.get(0));
-        tareaDesc.put("Desviar energía al reactor", salas.get(0));
-
-        // --- CAFETERÍA (Índice 1) ---
-        tareaDesc.put("Vaciar basurero", salas.get(1));
-        tareaDesc.put("Subir datos de cafetería", salas.get(1));
-        tareaDesc.put("Arreglar cableado de cafetería", salas.get(1));
-
-        // --- NAVEGACIÓN (Índice 2) ---
-        tareaDesc.put("Estabilizar dirección", salas.get(2));
-        tareaDesc.put("Alinear mapa", salas.get(2));
-        tareaDesc.put("Trazar rumbo espacial", salas.get(2));
-        tareaDesc.put("Descargar datos de navegación", salas.get(2));
-
-        // --- ELECTRICIDAD (Índice 3) ---
-        tareaDesc.put("Calibrar distribuidor", salas.get(3));
-        tareaDesc.put("Desviar energía principal", salas.get(3));
-        tareaDesc.put("Restablecer disyuntores", salas.get(3));
-        tareaDesc.put("Arreglar cableado de electricidad", salas.get(3));
-        tareaDesc.put("Descargar datos de electricidad", salas.get(3));
-
-        // --- ARMAMENTO (Índice 4) ---
-        tareaDesc.put("Destruir asteroides", salas.get(4));
-        tareaDesc.put("Calibrar cañones", salas.get(4));
-        tareaDesc.put("Descargar datos de armamento", salas.get(4));
-
-        // --- COMUNICACIONES (Índice 5) ---
-        tareaDesc.put("Reiniciar router WiFi", salas.get(5));
-        tareaDesc.put("Descargar datos de comunicaciones", salas.get(5));
-
-        int seleccion1 = 0;
-        int seleccion2 = 1;
-        ArrayList<String> keys = (ArrayList<String>) tareaDesc.keySet();
-        for (Tripulante tripulante : tripulantes) {
-            Tarea tarea1 = new Tarea(keys.get(seleccion1), tripulante, tareaDesc.get(keys.get(seleccion1)));
-            Tarea tarea2 = new Tarea(keys.get(seleccion2), tripulante, tareaDesc.get(keys.get(seleccion2)));
-            nave.agregarTarea(tarea1);
-            nave.agregarTarea(tarea2);
-            seleccion1 ++;
-            seleccion2 ++;
-        }
-
-        //TODO Generación de tareas: Crear y asignar tareas. Mínimo 2 por tripulante. Persistir.
-
-        while (nave.verificarVictoriaImpostor() || nave.verificarVictoriaTripulantes()) {
-            nave.turno();
-        }
-
-        if (nave.verificarVictoriaTripulantes()) {
-            System.out.println("============================================\n" +
-                    "        🎉  FIN DE LA PARTIDA  🎉\n" +
-                    "============================================");
-            System.out.println();
-            System.out.println("¡VICTORIA DE LOS TRIPULANTES!");
-
-            System.out.print("El impostor ");
             for (int i = 0; i < tripulantes.size(); i++) {
-                if (Objects.equals(tripulantes.get(i).getRol(), "Impostor")) {
-                    System.out.print(tripulantes.get(i).getNombre());
+                if (i%2 == 0) {
+                    Medico medico = new Medico(jugadores.removeFirst());
+                    tripulantes.add(medico);
+                } else {
+                    Ingeniero ingeniero = new Ingeniero(jugadores.removeFirst());
+                    tripulantes.add(ingeniero);
                 }
             }
-            System.out.println(" ha sido expulsado.");
-            System.out.println("La nave esta a salvo.");
-            System.out.println();
-            System.out.println("=== RESUMEN FINAL ===");
-            System.out.println("Tripulantes supervivientes:");
-            for (int i = 0; i < tripulantes.size(); i++) {
-                Tripulante trip = tripulantes.get(i);
 
-                String estado;
-                if (trip.getRol().equalsIgnoreCase("Impostor")) {
-                    estado = "Expulsado";
-                } else {
-                    if (trip.isVivo()) {
-                        estado = "Vivo";
+            ArrayList<Sala> salas = new ArrayList<>();
+            ArrayList<String> NomSalas = new ArrayList<>();
+            Collections.addAll(NomSalas, "Reactor", "Cafeteria", "Navegación", "Electricidad", "Armamento", "Comunicaciones");
+
+            for (int i = 0; i < NomSalas.size(); i++) {
+                Sala sala = new Sala(NomSalas.get(i));
+                salas.add(sala);
+            }
+
+
+            for (int i = 0; i < tripulantes.size(); i++) {
+                tripulanteDAO.insertar(tripulantes.get(i));
+            }
+
+            for (int i = 0; i < salas.size(); i++) {
+                salaDAO.insertar(salas.get(i));
+            }
+
+            Nave nave = new Nave(tripulantes, salas);
+
+            HashMap<String, Sala> tareaDesc = new HashMap<>();
+            // 0: Reactor, 1: Cafeteria, 2: Navegación, 3: Electricidad, 4: Armamento, 5: Comunicaciones
+
+            // --- REACTOR (Índice 0) ---
+            tareaDesc.put("Arrancar reactor", salas.get(0));
+            tareaDesc.put("Desbloquear colectores", salas.get(0));
+            tareaDesc.put("Desviar energía al reactor", salas.get(0));
+
+            // --- CAFETERÍA (Índice 1) ---
+            tareaDesc.put("Vaciar basurero", salas.get(1));
+            tareaDesc.put("Subir datos de cafetería", salas.get(1));
+            tareaDesc.put("Arreglar cableado de cafetería", salas.get(1));
+
+            // --- NAVEGACIÓN (Índice 2) ---
+            tareaDesc.put("Estabilizar dirección", salas.get(2));
+            tareaDesc.put("Alinear mapa", salas.get(2));
+            tareaDesc.put("Trazar rumbo espacial", salas.get(2));
+            tareaDesc.put("Descargar datos de navegación", salas.get(2));
+
+            // --- ELECTRICIDAD (Índice 3) ---
+            tareaDesc.put("Calibrar distribuidor", salas.get(3));
+            tareaDesc.put("Desviar energía principal", salas.get(3));
+            tareaDesc.put("Restablecer disyuntores", salas.get(3));
+            tareaDesc.put("Arreglar cableado de electricidad", salas.get(3));
+            tareaDesc.put("Descargar datos de electricidad", salas.get(3));
+
+            // --- ARMAMENTO (Índice 4) ---
+            tareaDesc.put("Destruir asteroides", salas.get(4));
+            tareaDesc.put("Calibrar cañones", salas.get(4));
+            tareaDesc.put("Descargar datos de armamento", salas.get(4));
+
+            // --- COMUNICACIONES (Índice 5) ---
+            tareaDesc.put("Reiniciar router WiFi", salas.get(5));
+            tareaDesc.put("Descargar datos de comunicaciones", salas.get(5));
+
+            int seleccion1 = 0;
+            int seleccion2 = 1;
+            ArrayList<String> keys = (ArrayList<String>) tareaDesc.keySet();
+            for (Tripulante tripulante : tripulantes) {
+                Tarea tarea1 = new Tarea(keys.get(seleccion1), tripulante, tareaDesc.get(keys.get(seleccion1)));
+                Tarea tarea2 = new Tarea(keys.get(seleccion2), tripulante, tareaDesc.get(keys.get(seleccion2)));
+                nave.agregarTarea(tarea1);
+                nave.agregarTarea(tarea2);
+                seleccion1 ++;
+                seleccion2 ++;
+            }
+
+            for (int i = 0; i < nave.getTareas().size(); i++) {
+                tareaDAO.insertar(nave.getTareas().get(i));
+            }
+
+            while (nave.verificarVictoriaImpostor() || nave.verificarVictoriaTripulantes()) {
+                nave.turno();
+            }
+
+            if (nave.verificarVictoriaTripulantes()) {
+                System.out.println("============================================\n" +
+                        "        🎉  FIN DE LA PARTIDA  🎉\n" +
+                        "============================================");
+                System.out.println();
+                System.out.println("¡VICTORIA DE LOS TRIPULANTES!");
+
+                System.out.print("El impostor ");
+                for (int i = 0; i < tripulantes.size(); i++) {
+                    if (Objects.equals(tripulantes.get(i).getRol(), "Impostor")) {
+                        System.out.print(tripulantes.get(i).getNombre());
+                    }
+                }
+                System.out.println(" ha sido expulsado.");
+                System.out.println("La nave esta a salvo.");
+                System.out.println();
+                System.out.println("=== RESUMEN FINAL ===");
+                System.out.println("Tripulantes supervivientes:");
+                for (int i = 0; i < tripulantes.size(); i++) {
+                    Tripulante trip = tripulantes.get(i);
+
+                    String estado;
+                    if (trip.getRol().equalsIgnoreCase("Impostor")) {
+                        estado = "Expulsado";
                     } else {
-                        estado = "Muerto";
+                        if (trip.isVivo()) {
+                            estado = "Vivo";
+                        } else {
+                            estado = "Muerto";
+                        }
+                    }
+
+                    System.out.printf("[%d] %-15s - %-10s - %s%n", i, trip.getNombre(), trip.getRol(), estado);
+                }
+
+                int tareasSinCompletar = 0;
+                for (int i = 0; i < nave.getTareas().size(); i++) {
+                    if (!nave.getTareas().get(i).isCompletada()) {
+                        tareasSinCompletar ++;
                     }
                 }
 
-                System.out.printf("[%d] %-15s - %-10s - %s%n", i, trip.getNombre(), trip.getRol(), estado);
+                System.out.println();
+                System.out.println("Tareas completadas: " + tareasSinCompletar + "/" + nave.getTareas().size());
             }
 
-            int tareasSinCompletar = 0;
-            for (int i = 0; i < nave.getTareas().size(); i++) {
-                if (!nave.getTareas().get(i).isCompletada()) {
-                    tareasSinCompletar ++;
+            if (nave.verificarVictoriaImpostor()) {
+                System.out.println("============================================\n" +
+                        "       💀  FIN DE LA PARTIDA  💀 \n" +
+                        "============================================");
+                System.out.println();
+                System.out.println("¡VICTORIA DEL IMPOSTOR!\n");
+
+                System.out.println(impostor.getNombre() + "(Impostor) ha conseguido eliminar a\n" +
+                        "suficientes tripulantes. La nave ha caido.");
+
+                System.out.println();
+                System.out.println("=== RESUMEN FINAL ===");
+                System.out.println("Tripulantes supervivientes:");
+                for (int i = 0; i < tripulantes.size(); i++) {
+                    Tripulante trip = tripulantes.get(i);
+
+                    String estado;
+                    if (trip.getRol().equalsIgnoreCase("Impostor")) {
+                        estado = "VIVO  😈";
+                    } else {
+                        estado = "Eliminado";
+                    }
+
+                    System.out.printf("[%d] %-15s - %-10s - %s%n", i, trip.getNombre(), trip.getRol(), estado);
                 }
             }
 
-            System.out.println();
-            System.out.println("Tareas completadas: " + tareasSinCompletar + "/" + nave.getTareas().size());
-        }
-
-        if (nave.verificarVictoriaImpostor()) {
-            System.out.println("============================================\n" +
-                    "       💀  FIN DE LA PARTIDA  💀 \n" +
-                    "============================================");
-            System.out.println();
-            System.out.println("¡VICTORIA DEL IMPOSTOR!\n");
-
-            System.out.println(impostor.getNombre() + "(Impostor) ha conseguido eliminar a\n" +
-                    "suficientes tripulantes. La nave ha caido.");
-
-            System.out.println();
-            System.out.println("=== RESUMEN FINAL ===");
-            System.out.println("Tripulantes supervivientes:");
-            for (int i = 0; i < tripulantes.size(); i++) {
-                Tripulante trip = tripulantes.get(i);
-
-                String estado;
-                if (trip.getRol().equalsIgnoreCase("Impostor")) {
-                    estado = "VIVO  😈";
-                } else {
-                    estado = "Eliminado";
-                }
-
-                System.out.printf("[%d] %-15s - %-10s - %s%n", i, trip.getNombre(), trip.getRol(), estado);
-            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
 
         System.out.println("Gracias por jugar!");
