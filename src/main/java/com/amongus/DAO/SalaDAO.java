9d9b5b457a94cea234e6fc9cc2073cfcb1bc9fcf
Rodrigo <rodriqet17@gmail.com>
2026-03-13 09:16:14 +0100

    Clases DAO completadas

--- src/main/java/com/amongus/DAO/SalaDAO.java
+++ src/main/java/com/amongus/DAO/SalaDAO.java
@@ -2,8 +2,18 @@ package com.amongus.DAO;
 
 import com.amongus.modelo.Sala;
 
+import java.util.ArrayList;
+
 public interface SalaDAO {
 
 
     void insertar(Sala sala);
+
+    Sala obtener(int id);
+
+    ArrayList<Sala> obtenerTodos();
+
+    void actualizar(Sala sala);
+
+    void eliminar(int id);
 }
--- src/main/java/com/amongus/DAO/TareaDAO.java
+++ src/main/java/com/amongus/DAO/TareaDAO.java
@@ -1,9 +1,21 @@
 package com.amongus.DAO;
 
+import com.amongus.modelo.Sala;
 import com.amongus.modelo.Tarea;
+import com.amongus.modelo.tripulante.Tripulante;
+
+import java.util.ArrayList;
 
 public interface TareaDAO {
 
 
-    void insertar(Tarea tarea);
+    void insertar(Tarea tarea, Tripulante tripulante, Sala sala);
+
+    Tarea obtener(int id);
+
+    ArrayList<Tarea> obtenerTodos();
+
+    void actualizar(Tarea tarea);
+
+    void eliminar(int id);
 }
